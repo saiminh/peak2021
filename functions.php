72d2495fbe169c7ad5747ec4e5d2417a78661a45
saiminh <saiminh@gmail.com>
2021-06-02 18:30:37 +0200

    versioning

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
 
 if ( ! defined( '_S_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '0.1.162' );
+	define( '_S_VERSION', '0.1.163' );
 }
 
 if ( ! function_exists( 'peak2021_setup' ) ) :
