98ddb6f10fda6a401b00b51469ba25c5cae9cb0d
saiminh <saiminh@gmail.com>
2021-06-02 17:46:36 +0200

    remove team coin

--- page-team.php
+++ page-team.php
@@ -18,9 +18,7 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-
-    <?php // get_template_part( 'inc/team', 'coin-header' ); ?>
-
+    
 		<?php
 		while ( have_posts() ) :
 			the_post();
