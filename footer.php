dc481c00c01dc302361dbb0e68cf14953943bc3d
githubpeak <91878352+githubpeak@users.noreply.github.com>
2021-10-07 11:38:47 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -24,7 +24,7 @@
         ?>
       </div>
       <div class="site-footer-column site-footer-social">
-          <header>Follow Us On:</header>
+          <header>Follow Us On</header>
           <a href="<?php the_global_var('crunchbase'); ?>" target="_blank">
             <?php get_template_part( 'inc/inline', 'footer-social-icons-crunchbase.svg' ); ?>
           </a>
@@ -40,9 +40,12 @@
           <a href="<?php the_global_var('twitter'); ?>" target="_blank">
             <?php get_template_part( 'inc/inline', 'footer-social-icons-twitter.svg' ); ?>
           </a>
+	      <BR><font size=3>Venture Capital</font>
+
       </div>
+	    
       <div class="site-footer-column site-footer-site-info">
-        <p>Peak HQ</p> 
+        <p>Peak</p> 
         <p>Capital C <br>
         Weesperplein 4b <br>
         1018 XA Amsterdam <br>
@@ -51,7 +54,7 @@
         <a href="mailto:<?php the_global_var('email'); ?>"><?php the_global_var('email'); ?></a></p>
       </div><!-- .site-info -->
       <div class="site-footer-column site-footer-site-mail">
-        <p>Mail:</p>
+        <p>Mail</p>
         <p>Eerste Jacob van Campenstraat 46-1 <br>
         1072 BG Amsterdam
       </div>
